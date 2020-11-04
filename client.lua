4a0965cb5a746911e699e0e125f4b5ee38cd9fa7
Dione Batista <dione1993@hotmail.com>
2020-11-04 09:56:23 -0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,4 +1,4 @@
-StaticSmitters = {
+StaticEmitters = {
 	'LOS_SANTOS_AMMUNATION_GUN_RANGE',
 	'LOS_SANTOS_VANILLA_UNICORN_01_STAGE',
 	'LOS_SANTOS_VANILLA_UNICORN_02_MAIN_ROOM',
@@ -7,7 +7,7 @@ StaticSmitters = {
 
 
 Citizen.CreateThread(function()
-    for index,emitter in pairs(StaticSmitters) do
+    for index,emitter in pairs(StaticEmitters) do
         SetStaticEmitterEnabled(emitter,false)
     end 
 end)
