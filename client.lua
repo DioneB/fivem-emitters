713e8e4e412659c6643d185fc4cce1ef329b1534
Dione Batista <dione1993@hotmail.com>
2020-11-04 12:28:29 +0000

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,13 @@
+StaticSmitters = {
+	'LOS_SANTOS_AMMUNATION_GUN_RANGE',
+	'LOS_SANTOS_VANILLA_UNICORN_01_STAGE',
+	'LOS_SANTOS_VANILLA_UNICORN_02_MAIN_ROOM',
+	'LOS_SANTOS_VANILLA_UNICORN_03_BACK_ROOM'
+}
+
+
+Citizen.CreateThread(function()
+    for index,emitter in pairs(StaticSmitters) do
+        SetStaticEmitterEnabled(emitter,false)
+    end 
+end)
